8c744e5148511f6a86db69a425159eff09bed9a0
Sávio <saviopf22@gmail.com>
2024-02-21 03:08:55 -0300

    finalização testes componente profile

---
+++ components/Profile/Profile.spec.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import Profile from "./index";
+
+describe("Profile tests", () => {
+  it("test request get data profile avatar", async () => {
+    const { findByTestId } = render(<Profile userName="savio777" />);
+
+    const imageRender = await findByTestId("avatar");
+
+    expect(imageRender._fiber.type).toEqual("Image");
+  });
+
+  it("test request get data profile name", async () => {
+    const { findByTestId } = render(<Profile userName="savio777" />);
+
+    const textName = await findByTestId("name");
+
+    expect(textName.props.children).toEqual("Sávio");
+  });
+});
